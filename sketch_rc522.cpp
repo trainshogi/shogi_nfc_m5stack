1e0de7db6a1cd514670b36548ab0975dbf94c87a
trainshogi <densyashogi@gmail.com>
2022-02-11 14:11:24 +0900

    Update sketch_rc522.cpp
    enable to use both LCD and rc522.

--- sketch_rc522.cpp
+++ sketch_rc522.cpp
@@ -1,73 +1,98 @@
 #include <SPI.h>
-#include <MFRC522.h>
-//#include <M5Stack.h>
+//#include <MFRC522.h>
+#include <M5Stack.h>
+
+#include <MFRC522v2.h>
+#include <MFRC522DriverSPI.h>
+#include <MFRC522DriverPinSimple.h>
+#include <MFRC522Debug.h>
+#include <MFRC522Constants.h>
+
+#define SS_1_PIN        21 //10         // Configurable, take a unused pin, only HIGH/LOW required, must be different to SS 2
+#define SS_2_PIN        22 //6          // Configurable, take a unused pin, only HIGH/LOW required, must be different to SS 1
+
+#define CK 5
+#define MI 17
+#define MO 16
 
 constexpr uint8_t RST_PIN = 33;//9;          // Configurable, see typical pin layout above
 
-#define SS_1_PIN        21 //10         // Configurable, take a unused pin, only HIGH/LOW required, must be different to SS 2
-#define SS_2_PIN        17 //6          // Configurable, take a unused pin, only HIGH/LOW required, must be different to SS 1
+SPIClass hspi(HSPI);
 
-#define NR_OF_READERS   2
+const SPISettings spiSettings = SPISettings(SPI_CLOCK_DIV4, MSBFIRST, SPI_MODE0); // May have to be set if hardware is not fully compatible to Arduino specifications.
 
 byte ssPins[] = {SS_1_PIN, SS_2_PIN};
 
-MFRC522 mfrc522[NR_OF_READERS];   // Create MFRC522 instance.
+MFRC522DriverPinSimple ss_1_pin(SS_1_PIN); // Configurable, take an unused pin, only HIGH/LOW required, must be different to SS 2.
+MFRC522DriverPinSimple ss_2_pin(SS_2_PIN); // Configurable, take an unused pin, only HIGH/LOW required, must be different to SS 1.
 
-SPIclass SPI2;
+MFRC522DriverSPI driver_1{ss_1_pin, hspi, spiSettings};
+MFRC522DriverSPI driver_2{ss_2_pin, hspi, spiSettings};
+
+MFRC522 readers[]{driver_1, driver_2};   // Create MFRC522 instance.
+
+//
+//
+//MFRC522 mfrc522[NR_OF_READERS];   // Create MFRC522 instance.
 
 void setup() {
-  Serial.begin(115200);   // Initialize serial communications with the PC
+
+
+  // m5stack初期化
+  M5.begin();
+  // QR Code
+  M5.Lcd.qrcode("content",50,10,220,6);
+
+  
+//  Serial.begin(115200);   // Initialize serial communications with the PC
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
   digitalWrite(SS_1_PIN,HIGH);
   digitalWrite(SS_2_PIN,HIGH);
-  SPI2.begin();      // Init SPI bus
 
-  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
-    digitalWrite(ssPins[reader], LOW);
-    mfrc522[reader].PCD_Init(ssPins[reader], RST_PIN); // Init each MFRC522 card
+  hspi.begin(CK,MI,MO);      // Init SPI bus
+  static uint8_t i = 0;
+
+  for (MFRC522 reader : readers) {
+    digitalWrite(ssPins[i], LOW);
+    reader.PCD_Init(); // Init each MFRC522 card.
     Serial.print(F("Reader "));
-    Serial.print(reader);
+    Serial.print(i);
     Serial.print(F(": "));
-    mfrc522[reader].PCD_DumpVersionToSerial();    
-    digitalWrite(ssPins[reader], HIGH);
+    MFRC522Debug::PCD_DumpVersionToSerial(reader, Serial);    
+    digitalWrite(ssPins[i], HIGH);
+    i++;
   }
-
-  // m5stack初期化
-//  M5.begin();
-  // QR Code
-//  M5.Lcd.qrcode("content",50,10,220,6);
 }
 
 void loop() {
   String nfcdata;
+  uint8_t i = 0;
 
-  for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
+  for (MFRC522 reader : readers) {
     // Look for new cards
-    digitalWrite(ssPins[reader], LOW);
+    digitalWrite(ssPins[i], LOW);
 
-     if (mfrc522[reader].PICC_IsNewCardPresent() && mfrc522[reader].PICC_ReadCardSerial()) {
+     if (reader.PICC_IsNewCardPresent() && reader.PICC_ReadCardSerial()) {
       Serial.print(F("Reader "));
-      Serial.print(reader);
+      Serial.print(i);
       // Show some details of the PICC (that is: the tag/card)
       Serial.print(F(": Card UID:"));
-      dump_byte_array(mfrc522[reader].uid.uidByte, mfrc522[reader].uid.size);
+      dump_byte_array(reader.uid.uidByte, reader.uid.size);
       Serial.println();
       Serial.print(F("PICC type: "));
-      MFRC522::PICC_Type piccType = mfrc522[reader].PICC_GetType(mfrc522[reader].uid.sak);
-      Serial.println(mfrc522[reader].PICC_GetTypeName(piccType));
-      // PICC_DumpMifareUltralightToSerial(reader);
+      MFRC522::PICC_Type piccType = reader.PICC_GetType(reader.uid.sak);
+      Serial.println(MFRC522Debug::PICC_GetTypeName(piccType));
       nfcdata = PICC_DumpMifareUltralightToSerialArray(reader);
-      Serial.println(nfcdata);
-
-      
+      Serial.println(nfcdata);      
 
       // Halt PICC
-      mfrc522[reader].PICC_HaltA();
+      reader.PICC_HaltA();
       // Stop encryption on PCD
-      mfrc522[reader].PCD_StopCrypto1();
+      reader.PCD_StopCrypto1();
      } //if (mfrc522[reader].PICC_IsNewC
     
-    digitalWrite(ssPins[reader], HIGH);
+    digitalWrite(ssPins[i], HIGH);
+    i++;
 
   } //for(uint8_t reader
 
@@ -84,51 +109,9 @@ void dump_byte_array(byte *buffer, byte bufferSize) {
 }
 
 /**
- * Dumps memory contents of a MIFARE Ultralight PICC.
- */
-void PICC_DumpMifareUltralightToSerial(uint8_t reader) {
-  MFRC522::StatusCode status;
-  byte byteCount;
-  byte buffer[18];
-  byte i;
-  
-  Serial.println(F("Page  0  1  2  3"));
-  // Try the mpages of the original Ultralight. Ultralight C has more pages.
-  for (byte page = 4; page < 16; page +=4) { // Read returns data for 4 pages at a time.
-    // Read pages
-    byteCount = sizeof(buffer);
-    status = mfrc522[reader].MIFARE_Read(page, buffer, &byteCount);
-    if (status != MFRC522::STATUS_OK) {
-      Serial.print(F("MIFARE_Read() failed: "));
-      Serial.println(MFRC522::GetStatusCodeName(status));
-      break;
-    }
-    // Dump data
-    for (byte offset = 0; offset < 4; offset++) {
-      i = page + offset;
-      if(i < 10)
-        Serial.print(F("  ")); // Pad with spaces
-      else
-        Serial.print(F(" ")); // Pad with spaces
-      Serial.print(i);
-      Serial.print(F("  "));
-      for (byte index = 0; index < 4; index++) {
-        i = 4 * offset + index;
-        if(buffer[i] < 0x10)
-          Serial.print(F(" 0"));
-        else
-          Serial.print(F(" "));
-        Serial.print(buffer[i], HEX);
-      }
-      Serial.println();
-    }
-  }
-} // End PICC_DumpMifareUltralightToSerial()
-
-/**
  * Convert memory contents of a MIFARE Ultralight PICC to String.
  */
-String PICC_DumpMifareUltralightToSerialArray(uint8_t reader) {
+String PICC_DumpMifareUltralightToSerialArray(MFRC522 reader) {
   MFRC522::StatusCode status;
   byte byteCount;
   byte buffer[18];
@@ -136,10 +119,10 @@ String PICC_DumpMifareUltralightToSerialArray(uint8_t reader) {
   
   // Read returns data for 4 pages at a time.
   byteCount = sizeof(buffer);
-  status = mfrc522[reader].MIFARE_Read(page, buffer, &byteCount);
-  if (status != MFRC522::STATUS_OK) {
+  status = reader.MIFARE_Read(page, buffer, &byteCount);
+  if (status != MFRC522Constants::STATUS_OK) {
     Serial.print(F("MIFARE_Read() failed: "));
-    Serial.println(MFRC522::GetStatusCodeName(status));
+    Serial.println(MFRC522Debug::GetStatusCodeName(status));
     return "";
   }
   if ((buffer[0] != 0x03) || (buffer[2] != 0xD1) || (buffer[3] != 0x01) || (buffer[5] != 0x54)) {
